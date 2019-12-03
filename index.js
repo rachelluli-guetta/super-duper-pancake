346fd17166e66be0951abea3dc70a2646a357a8a
Rachel Guetta <rguetta@yotpo.com>
2019-12-03 13:16:31 +0100

    run ncc

--- index.js
+++ index.js
@@ -4,19 +4,7 @@ const wait = require('./wait');
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+  console.log('yeyyy')
 }
 
 run()
